e11a5edb8e58a8dc13c938aac254cb9ee5dc79ed
joel3 <joel3@JOEL>
2024-11-10 15:13:38 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_uppgift_7_OOP_Generic_Joel
+{
+    public class Employee
+    {
+        public Employee(string id, string name, string gender, decimal salary)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Gender: {Gender}, Salary: {Salary}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Lab_uppgift_7_OOP_Generic_Joel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("001", "Bob", "Male", 32000m);
+            Employee employee2 = new Employee("002", "Anders", "Male", 40000m);
+            Employee employee3 = new Employee("003", "Hanna", "Female", 24000m);
+            Employee employee4 = new Employee("004", "Martin", "Male", 30000m);
+            Employee employee5 = new Employee("005", "Linda", "Female", 27000m);
+            Stack<Employee> stack = new Stack<Employee>();
+            stack.Push(employee1);
+            stack.Push(employee2);
+            stack.Push(employee3);
+            stack.Push(employee4);
+            stack.Push(employee5);
+            int totalEmployees = stack.Count;
+            foreach (Employee emp in stack)
+            {
+                Console.WriteLine(emp.ToString());
+
+                Console.WriteLine($"There is {totalEmployees} in the stack.");
+            }
+
+            Console.WriteLine();
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine($"There is {stack.Count} left in the stack.");
+            }
+
+            stack.Push(employee1);
+            stack.Push(employee2);
+            stack.Push(employee3);
+            stack.Push(employee4);
+            stack.Push(employee5);
+            Console.WriteLine($"There is {stack.Peek()} in the stack. ");
+
+            Console.WriteLine();
+            if (stack.Contains(employee3))
+            {
+                Console.WriteLine($"{employee3.ToString()} is in the stack.");
+            }
+            else
+            {
+                Console.WriteLine($"{employee3.ToString()} is not in the stack.");
+            }
+
+            List<Employee> employees = new List<Employee>
+            {
+                employee1, employee2, employee3, employee4, employee5
+            };
+
+            Console.WriteLine();
+            if (employees.Contains(employee2))
+            {
+                Console.WriteLine($"{employee2.ToString()} is in the list.");
+            }
+            else
+            {
+                Console.WriteLine($"{employee2.ToString()} is not in the list.");
+            }
+            Console.WriteLine();
+            var firstMaleEmployee = employees.Find(e => e.Gender == "Male");
+
+            Console.WriteLine($"The first male employee in the list is {firstMaleEmployee}. ");
+            Console.WriteLine();
+            var allMaleEmployees = employees.FindAll(m => m.Gender == "Male");
+
+            Console.WriteLine("All men in the list.");
+            foreach (Employee employee in allMaleEmployees)
+            { Console.WriteLine(employee.ToString()); };
+        }
+    }
+}
